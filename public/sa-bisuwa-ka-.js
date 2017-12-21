8af08e48ebc4f7260a6be97fd6374fb6539ea522
you-hei <nakazato.2000@gmail.com>
2017-12-22 00:35:03 +0900

    cacheしたindex.htmlをfetchで返す

--- public/sa-bisuwa-ka-.js
+++ public/sa-bisuwa-ka-.js
@@ -9,25 +9,35 @@ self.addEventListener('install', (event) => {
   )
 })
 
-// self.addEventListener('fetch', (event) => {
-//   console.log('fetch', event)
-//   if (event.request.url === 'http://localhost:8888/ocnu.json') {
-//     console.log('ocnuを取ろうとしてるぞ！')
-//     event.respondWith(new Promise((resolve) => {
-//       // 返すjson
-//       const obj = {
-//         o: {
-//           name: 'hoge-',
-//           color: '#000000',
-//           nagasa: 2000
-//         }
-//       }
-//       // Blob オブジェクトはファイルに似たオブジェクトで、immutable な生データです。(from MDN)
-//       const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' })
-//       // responseにblobとステータスとかをつける
-//       const response = new Response(blob, { 'status': 200, 'statusText': 'yesyesyes!' })
-//       // 返す
-//       resolve(response)
-//     }))
-//   }
-// })
+self.addEventListener('fetch', (event) => {
+  console.log('fetch', event)
+  if (event.request.url === 'http://localhost:8888/') {
+    event.respondWith(caches.match(event.request, { cacheName: CACHE_VERSION})
+      .then((response) => {
+        if (response) {
+          return response
+        }
+        // なければ普通にfetchして返す
+        return fetch(event.request)
+      }))
+  }
+  // if (event.request.url === 'http://localhost:8888/ocnu.json') {
+  //   console.log('ocnuを取ろうとしてるぞ！')
+  //   event.respondWith(new Promise((resolve) => {
+  //     // 返すjson
+  //     const obj = {
+  //       o: {
+  //         name: 'hoge-',
+  //         color: '#000000',
+  //         nagasa: 2000
+  //       }
+  //     }
+  //     // Blob オブジェクトはファイルに似たオブジェクトで、immutable な生データです。(from MDN)
+  //     const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' })
+  //     // responseにblobとステータスとかをつける
+  //     const response = new Response(blob, { 'status': 200, 'statusText': 'yesyesyes!' })
+  //     // 返す
+  //     resolve(response)
+  //   }))
+  // }
+})
