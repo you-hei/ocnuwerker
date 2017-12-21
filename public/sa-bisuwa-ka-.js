4231fca23916598de80393ef521826e0cb3ac018
you-hei <nakazato.2000@gmail.com>
2017-12-21 23:03:57 +0900

    特定のリクエストをインターセプトしてカスタムレスポンスを返す

--- public/sa-bisuwa-ka-.js
+++ public/sa-bisuwa-ka-.js
@@ -6,7 +6,21 @@ self.addEventListener('fetch', (event) => {
   console.log('fetch', event)
   if (event.request.url === 'http://localhost:8888/ocnu.json') {
     console.log('ocnuを取ろうとしてるぞ！')
-    event.respondWith(fetch(event.request))
+    event.respondWith(new Promise((resolve) => {
+      // 返すjson
+      const obj = {
+        o: {
+          name: 'hoge-',
+          color: '#000000',
+          nagasa: 2000
+        }
+      }
+      // Blob オブジェクトはファイルに似たオブジェクトで、immutable な生データです。(from MDN)
+      const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' })
+      // responseにblobとステータスとかをつける
+      const response = new Response(blob, { 'status': 200, 'statusText': 'yesyesyes!' })
+      // 返す
+      resolve(response)
   }
 
 })
