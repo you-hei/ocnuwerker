51e2ec83801fd58554b8df1aa1ef991c389eca6c
you-hei <nakazato.2000@gmail.com>
2018-02-22 20:39:41 +0900

    基本的なやつら

---
+++ public/sa-bisuwa-ka-.js
@@ -0,0 +1,5 @@
+const CACHE_VERSION= 'v1.0.0'
+
+self.addEventListener('install', (event) => {
+  console.log('installed!')
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sa-bisuwa-ka-.js')
+    .then((registration) => {
+      console.log('registered')
+    })
+    .catch((err) => {
+      console.log('register failed')
+    })
+}
+
 render(
   <div><App /></div>,
   document.getElementById('root')
