62955e9c687d94e0af00708b3e2ea10728fc6d8a
you-hei <nakazato.2000@gmail.com>
2018-02-22 23:42:34 +0900

    pushを受け取ったらnotification

--- public/sa-bisuwa-ka-.js
+++ public/sa-bisuwa-ka-.js
@@ -6,4 +6,10 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('push', (event) => {
   console.log('push イヴェントを受け取ったわ！ほんまやで！', event)
+  if (self.Notification && self.Notification.permission === 'granted') {
+    const dataJson = event.data.json()
+    const { title } = dataJson
+    console.log(`title: ${title}, json: ${JSON.stringify(dataJson)}`)
+    event.waitUntil(self.registration.showNotification(title, dataJson))
+  }
 })
