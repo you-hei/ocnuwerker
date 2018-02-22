0678c5e34680acb0aba6372644e70c981aaecb51
you-hei <nakazato.2000@gmail.com>
2018-02-22 22:55:41 +0900

    pushイベントを受け取れるようにする

--- public/sa-bisuwa-ka-.js
+++ public/sa-bisuwa-ka-.js
@@ -3,3 +3,7 @@ const CACHE_VERSION= 'v1.0.0'
 self.addEventListener('install', (event) => {
   console.log('installed!')
 })
+
+self.addEventListener('push', (event) => {
+  console.log('push イヴェントを受け取ったわ！ほんまやで！', event)
+})
