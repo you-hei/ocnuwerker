abb6a1070fce77c672697ea0f3564086b907a39a
you-hei <nakazato.2000@gmail.com>
2018-02-22 21:42:22 +0900

    push チェック

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  pushチェック() {
+    console.log('pushチェック')
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistration()
+        .then((swr: ServiceWorkerRegistration) => {
+          if ('pushManager' in swr) {
+            console.log('push 使えるよーん')
+          } else {
+            console.log('残念！push使えませーん')
+          }
+        })
+    } else {
+      console.log('私はサービスワーカーさえ使えないブラウザです')
+    }
+  }
+
   render() {
     return (
-      <div>Appですよ</div>
+      <div>
+        Appですよ
+        <div>
+          <input type="button" value="pushが使えるかチェックしてみよう" onClick={this.pushチェック} />
+        </div>
+      </div>
     )
   }
 }
